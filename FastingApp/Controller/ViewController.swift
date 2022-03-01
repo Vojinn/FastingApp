0ce4d0dc9ed0b8bfe0e6324f9984966b58d48acb
Vojin <dvojinovic38@gmail.com>
2022-03-01 22:46:40 +0100

    Initial Commit

---
+++ FastingApp/Controller/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FastingApp
+//
+//  Created by Dusan Vojinovic on 1.3.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
